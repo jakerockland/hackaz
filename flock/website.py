5be806c3aa698563629fa47676a39d172fdc3317
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-07 04:48:24 -0700

    fuckyall

--- flock/website.py
+++ flock/website.py
@@ -3,3 +3,7 @@ from flock import app
 @app.route("/")
 def hello():
     return "Hello, I love Digital Ocean!"
+
+@app.route("/sqldatabase")
+def oh_vip():
+    return "Look at this pretty database"
