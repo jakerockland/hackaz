5236abc66c905109019dbbb91d6a4bf85f4f5444
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-07 05:44:36 -0700

    added index

--- flock/website.py
+++ flock/website.py
@@ -12,10 +12,10 @@ from flask.ext.assets import Environment, Bundle
 # custom module imports
 from flock import app
 
-@app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean!"
-
-@app.route("/about")
-def oh_vip():
-    return "About page bby"
+@app.route('/', defaults={'page': 'index'})
+@app.route('/<page>')
+def show(page):
+    try:
+        return render_template('%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
