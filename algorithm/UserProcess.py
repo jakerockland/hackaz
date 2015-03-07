a2e584fa8b6632f2893aa5cfe846f227a1e616ff
kuba1 <kubastechly@gmail.com>
2015-03-07 08:56:53 -0700

    Set up UserProcess, wrote init and calculate functions.

--- algorithm/UserProcess.py
+++ algorithm/UserProcess.py
@@ -2,20 +2,46 @@ import data.py # Get our horrendous dictionary of hashtag pairs with correspondi
 
 class UserProcess:
 
-	def __init__(usersTweets,username):
+	def __init__(users_tweets,username):
+		# usersTweets is a dictionary.
 		# set up empty list and dictionary for later output.
 		self.to_follow=[] 
-		related_tags={}
-	 	
+		self.related_tags={}
+
 		# calculate all the things, so that we can then get results when necessary.		
-		self.calculate(usersTweets,username)
+		self.calculate(users_tweets,username)
 		
-	def calculate(usersTweets,username):
+	def calculate(users_tweets,username):
 		# Calculates the users similarities with others
 		# Is only internal, returns nothing
-		pass 
 
-	def calc_weights(userTweets):
+		# Get user's weights
+		username_weights = calc_weights(users_tweets[username])
+		
+		# Pull out username's tweets. Del from dictionary to speed up for loop.
+		username_tweets = users_tweets[username]
+		del users_tweets[username]
+		
+		temp_list = []
+
+		# Loop through all users and calculate "distance" with username.
+		for match in users_tweets:
+			match_weights=calc_weights(users_tweets[match])
+			distance = weight_diff(username_weights,match_weights)
+
+			# Check if match in top 10 matches; add if it is.
+			if len(temp_list)>=10 and distance<temp_list[-1][1]:
+				del temp_list[-1]
+				temp_list.append((match,distance))
+				temp_list.sort(key=lambda tup: tup[1]) # Sorts by second element.
+			elif len(temp_list)<10:
+				self.to_follow.append((match,distance))
+				temp_list.sort(key=lambda tup: tup[1])
+		
+		# Save to_follow for later
+		self.to_follow = list(zip(*temp_list)[0])
+
+	def calc_weights(user_tweets):
 		# Calculates weights for each node by adding users adjusted percentage
 		# to the delta-adjusted percentages of similar nodes and subtracting
 		# the delta-adjusted overcounted weights
