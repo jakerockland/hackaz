82835a3ac9a7108f60e3203cb99b3e3eac7d8191
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-07 05:35:53 -0700

    added imports

--- flock/website.py
+++ flock/website.py
@@ -1,9 +1,21 @@
+# standard library imports
+import os
+import uuid
+import json
+import httplib2
+
+# Flask and web server imports
+from flask import render_template, redirect, url_for, abort, request, flash, jsonify
+from jinja2 import TemplateNotFound
+from flask.ext.assets import Environment, Bundle
+
+# custom module imports
 from flock import app
 
 @app.route("/")
 def hello():
     return "Hello, I love Digital Ocean!"
 
-@app.route("/sqldatabase")
+@app.route("/about")
 def oh_vip():
-    return "Look at this pretty database"
+    return "About page bby"
