58f3048fce3ebef99d68555231f1894a5b1dd215
gilli17 <gillilandalexandra@gmail.com>
2015-03-07 23:52:41 -0700

    Not much has changed

--- api_testing.py
+++ api_testing.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = "ZW9fy0Sz9dLS9LilRJK1fdMIO"
-consumer_secret = "tRgKWz9jMSenWL3ZIwr0aIETkXwpLB64uCZcVdc6Q1mHKTBxDL"
-access_token = "2585352386-6JtnsjTdCJZkJhKsaImfGHhPxZJLo1YsGDPJIS7"
-access_token_secret = "utdjm1NM8br5yncH14KdgvefZEIpFfTV5Ch3L4Yo3JEpS"
+consumer_key = "SqwvX6h8i9TfrwMJfWK3PHv5L"
+consumer_secret = "gsAxqf5RyQQOEKyl0gq9AOKfFo8dDEWMIvU1aNQJv2sfKo758o"
+access_token = "2585352386-PiCcgwiYciO2mfPGxTaMLXsPD47EAlUZamucJ6z"
+access_token_secret = "9082pHXLQyadkuFMzqdy8Hq3SMDPOtSGaoW7HWSoY1qgB"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -11,12 +11,12 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 twitter_id = "nfl" # can use us id number or screen name to call data
-#api.friends_ids(twitter_id)[1:200]
+print api.friends_ids(twitter_id)[0:4] # 200 people the user follows
 
-calling_first_tweet = api.user_timeline(twitter_id,count=100)[0]
+#calling_first_tweet = api.user_timeline(twitter_id,count=100)[0]
 
-first_cut = str(calling_first_tweet).split("text=u")[1]
+#first_cut = str(calling_first_tweet).split("text=u")[1]
 # line above cuts information before tweet
 
-print str(first_cut).split("in_reply_to_status_id=")[0]
-# line above cuts after tweet and corresponding link 
+#print str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
