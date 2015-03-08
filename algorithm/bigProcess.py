faca988062d847c8beaebe16220885d0bbc457e3
kuba1 <kubastechly@gmail.com>
2015-03-08 08:58:28 -0700

    more changes

--- algorithm/bigProcess.py
+++ algorithm/bigProcess.py
@@ -9,12 +9,21 @@ class BigProcess(object):
 	def __init__(self, tweets_info):
 		# Initializes the process. Searches through a bunch of tweets and builds
 		# a model, consisting of "probabilities", "deltas", and "user_percent"
+		
+		print "Initializing BigProcess"
+		print "Stripping tweets and getting rid of uppercase and underscores."
 		tweets = self.strip_basic_tweets(tweets_info)
+		print "Getting tweet percentages."
 		self.percentages = tweets_percentage(tweets)
+		print "Getting two-way tweet probabilities."
 		self.probabilities = self.calc_probabilities(tweets)
+		print "Getting deltas."
 		self.deltas = self.calc_deltas()
+		print "Getting per user percent adjustments."
 		self.user_percent = self.percentages
+		print "Writing file."
 		self.write_file()
+		print "BigProcess Initialized."
 
 	def strip_basic_tweets(self,tweets_info):
 		tweets = []
@@ -24,30 +33,40 @@ class BigProcess(object):
 		return tweets # TODO make this take in twitter status objects and spit out just the text of the tweets.
 
 	def graph(self):
+		print "Graphing..."
 		G = nx.Graph()
+		nx.draw(G,with_labels=False)
 		for delta in self.deltas:
+			#print "Graphing "+str(delta)+"."
 			split_lines = delta.split("|")
 			first = split_lines[0]
 			second = split_lines[1]
 			G.add_edge(first,second,weight=self.deltas[delta])
-		
+		print "Arranging graph"
 		pos=nx.spring_layout(G)
 		
-		nx.draw_networkx_nodes(G,pos,node_size=100)
-
+		print "Coloring and prettifying graph."
 		for delta in self.deltas:
 			split_lines = delta.split("|")
 			first = split_lines[0]
 			second = split_lines[1]
-			w = 5*self.deltas[delta]**(0.5)
+			w = 5*self.deltas[delta]**(1)
 			a = self.deltas[delta]
-			nx.draw_networkx_edges(G,pos,edgelist=[(first,second)],width=w,alpha=a,edge_color='b')
-
-		nx.draw_networkx_labels(G,pos,font_size=10,font_family='open sans')
+			nx.draw_networkx_edges(G,pos,edgelist=[(first,second)],width=w,alpha=a,edge_color='#8ccfff')
 		
+		percents_to_rank = self.percentages.values()
+		max_percent=max(percents_to_rank)
+		
+		print "Drawing graph labels."
+		for node in self.percentages:
+			f = self.percentages[node]**(0.5)*16/max_percent
+			print node, f
+			# nx.draw_networkx_nodes(G,pos,nodelist=[node],node_size=n, node_color='#55acee')
+			nx.draw_networkx_labels(G,pos,{node:node},font_size=f,font_family='Source Sans Pro',font_color='#333333')
+
 		plt.axis('off')
 		plt.savefig("weighted_graph.png")
-		plt.show()
+		# plt.show()
 
 	def calc_deltas(self):
 		# Takes probabilities, and does a sum across all neighbors of each 
@@ -57,6 +76,7 @@ class BigProcess(object):
 		
 		hashtag_size=float(len(self.percentages))
 		for tag1 in self.percentages:
+			#print "Calculating deltas across "+str(tag1)+"."
 			for tag2 in self.percentages:
 				if tag1 < tag2:
 					terms = []
@@ -64,15 +84,21 @@ class BigProcess(object):
 					for y in self.percentages:
 						terms.append(abs(self.probabilities[y+"|"+tag1]-self.probabilities[y+"|"+tag2]))
 						denom.append(self.probabilities[y+"|"+tag1]+self.probabilities[y+"|"+tag2])
-					deltas[tag1+"|"+tag2]=1-(sum(terms)/sum(denom))**2
-					print tag1+"|"+tag2+": "+str(1-(sum(terms)/sum(denom))**2)
+					if sum(denom)!=0:
+						deltas[tag1+"|"+tag2]=1-(sum(terms)/float(sum(denom)))**2
+					else: 
+						print tag1+"|"+tag2+"+"+str(sum(terms))+"+"+str(sum(denom))
+						deltas[tag1+"|"+tag2]=0
+					#print tag1+"|"+tag2+": "+str(1-(sum(terms)/sum(denom))**2)+"+"+str(sum(terms))+"+"+str(sum(denom))
 		return deltas
 
 	def calc_probabilities(self,tweets):
 		# Calculates P(tag1|tag2)'s for all pairs of hashtags tag1,tag2
 		probabilities = {}
 		for tag2 in self.percentages:
+			#print "Calculating probabilities across "+str(tag2)+"."
 			conditional_percents=tweets_percentage(tweets,tag2)
+			print conditional_percents
 			for tag1 in self.percentages:
 				if tag1 in conditional_percents:
 					probabilities[tag1+"|"+tag2]=conditional_percents[tag1]
