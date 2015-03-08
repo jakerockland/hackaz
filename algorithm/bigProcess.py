efaf59029870a67e4a22f317b1f8dd0b5dd8398a
kuba1 <kubastechly@gmail.com>
2015-03-07 20:02:47 -0700

    Fixed algorithm; all should run now

--- algorithm/bigProcess.py
+++ algorithm/bigProcess.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from userProcess import *
+from tweets_percentage import *
 
 class BigProcess(object):
 	
 	def __init__(self, tweets):
 		# Initializes the process. Searches through a bunch of tweets and builds
 		# a model, consisting of "probabilities", "deltas", and "user_percent"
-		self.percentages = userProcess.tweets_percentage(tweets)
-		self.probabilities = calc_probabilities(tweets)
+		self.percentages = tweets_percentage(tweets)
+		self.probabilities = self.calc_probabilities(tweets)
 		self.deltas = self.calc_deltas()
+		self.user_percent = self.percentages
 		self.write_file()
-		self.user_percent={}
+
 
 	def calc_deltas(self):
 		# Takes probabilities, and does a sum across all neighbors of each 
@@ -19,23 +20,29 @@ class BigProcess(object):
 		# These are symmetric, so we only need to do half.
 		deltas = {}
 		
-		hashtag_size=len(self.percentages)
+		hashtag_size=float(len(self.percentages))
 		for tag1 in self.percentages:
 			for tag2 in self.percentages:
 				if tag1 < tag2:
 					terms = []
+					denom = []
 					for y in self.percentages:
-						terms.append(1-(self.probabilities[tag1+"|"+tag2]-self.probabilities[tag1+"|"+tag2])^2/float(hashtag_size))
-					deltas[tag1+"|"+tag2]=sum(terms)
+						terms.append(abs(self.probabilities[y+"|"+tag1]-self.probabilities[y+"|"+tag2]))
+						denom.append(self.probabilities[y+"|"+tag1]+self.probabilities[y+"|"+tag2])
+					deltas[tag1+"|"+tag2]=1-(sum(terms)/sum(denom))**2
+					print tag1+"|"+tag2+": "+str(1-(sum(terms)/sum(denom))**2)
 		return deltas
 
 	def calc_probabilities(self,tweets):
 		# Calculates P(tag1|tag2)'s for all pairs of hashtags tag1,tag2
 		probabilities = {}
 		for tag2 in self.percentages:
-			conditional_percents=userProcess.tweets_percentage(tweets,tag2)
-			for tag1 in conditional_percents:
-				probabilities[tag1+"|"+tag2]=conditional_percents[tag1]
+			conditional_percents=tweets_percentage(tweets,tag2)
+			for tag1 in self.percentages:
+				if tag1 in conditional_percents:
+					probabilities[tag1+"|"+tag2]=conditional_percents[tag1]
+				else:
+					probabilities[tag1+"|"+tag2]=0
 
 		return probabilities
 
@@ -43,11 +50,11 @@ class BigProcess(object):
 		# Write a giant, sketchy dictionary.
 		with open('data.py', 'w') as the_file:
 			the_file.write('deltas = ')
-			the_file.write(self.delta)
+			the_file.write(str(self.deltas))
 			the_file.write('\n')
 			the_file.write('probabilities = ')
-			the_file.write(self.probabilities)
+			the_file.write(str(self.probabilities))
 			the_file.write('\n')
 			the_file.write('user_percent = ')
-			the_file.write(self.user_percent)
+			the_file.write(str(self.user_percent))
 			the_file.write('\n')
