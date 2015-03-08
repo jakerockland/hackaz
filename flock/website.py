450e5761a0b5d69b6a8c8cdb87438495fac998cb
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-08 06:33:08 -0700

    ASDF

--- flock/website.py
+++ flock/website.py
@@ -45,13 +45,13 @@ def show(page):
     except TemplateNotFound:
         abort(404)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+#
+# @app.errorhandler(500)
+# def page_not_found(e):
+#     return render_template('500.html'), 500
 
 
 """
