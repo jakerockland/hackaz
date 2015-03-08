7795c7b795024dfd086a9e57a5188edf13a0ff0b
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-07 20:12:13 -0700

    updated oauth 2....

--- flock/oauth.py
+++ flock/oauth.py
@@ -12,14 +12,13 @@ oauth = OAuth(app) # create OAuth container
 twitter_config = config.oauth.get('twitter', {})
 twitter_auth = oauth.remote_app(
     "twitter",
-    access_token_method='get',
+    access_token_method='GET',
     **twitter_config
 )
 
 
 """
-Return a URL to redirect the user to after authenticating with a third-party
-application like Facebook or Twitter.
+Return a URL to redirect the user to after authenticating with Twitter
 """
 def get_next_url():
     return request.args.get('next') or request.referrer or None
@@ -31,7 +30,7 @@ as the storage backend (data stored as an ecrypted cookie in the browser).
 """
 @twitter_auth.tokengetter
 def get_twitter_token(token=None):
-    return session.get('twitter_token',(None, None))
+    return session.get('twitter_token', (None, None))
 
 
 """
@@ -39,29 +38,31 @@ Provide a URL endpoint that allows a user to log in to Twitter to authorize
 access to their account via OAuth. This handler returns a redirect to the
 Twitter web site.
 """
-@app.route('/login')
-def login():
-    return twitter_auth.authorize(callback=url_for('oauth_authorized',
-        next=request.args.get('next') or request.referrer or None))
+@app.route('/login/twitter')
+def login_twitter():
+    return twitter_auth.authorize(
+        callback=url_for(
+            'oauth_authorized_twitter',
+            next=get_next_url(),
+            _external=True
+        )
+    )
 
 
 """
 Provide a URL endpoint that the user will visit after authorizing Flock
 as a client application for their Twitter account.
 """
-@app.route('/oauth-authorized')
+@app.route('/oauth-authorized/twitter')
 @twitter_auth.authorized_handler
-def oauth_authorized(resp):
-    next_url = request.args.get('next') or url_for('index')
-    if resp is None:
-        flash(u'You denied the request to sign in.')
-        return redirect(next_url)
+def oauth_authorized_twitter(authentication):
+    if authentication is None:
+        flash(u'We did not receive authorization for your Twitter account.')
+    else:
+        flash(u'Successfully authenticated with Twitter.')
+        session['twitter_token'] = (
+            authentication['oauth_token'],
+            authentication['oauth_token_secret']
+        )
 
-    session['twitter_token'] = (
-        resp['oauth_token'],
-        resp['oauth_token_secret']
-    )
-    session['twitter_user'] = resp['screen_name']
-
-    flash('You were signed in as %s' % resp['screen_name'])
-    return redirect(next_url)
+    return redirect(request.args.get('next') or '/')
