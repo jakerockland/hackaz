5f514a8777f92d4b07dbf0334cac00d0c55e05cc
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-08 06:19:03 -0700

    ASDFDSF

--- flock/website.py
+++ flock/website.py
@@ -47,11 +47,11 @@ def show(page):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('notfound.html'), 404
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('heavylifting.html'), 500
+    return render_template('500.html'), 500
 
 
 """
