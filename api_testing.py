802362dbf864525863f1f8ec66fce370edc767cd
gilli17 <gillilandalexandra@gmail.com>
2015-03-08 02:42:44 -0700

    Manually pulling 10 tweets

--- api_testing.py
+++ api_testing.py
@@ -10,13 +10,89 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+save = ''
+
 twitter_id = "nfl" # can use us id number or screen name to call data
-print api.friends_ids(twitter_id)[0:4] # 200 people the user follows
+api.friends_ids(twitter_id)[0:2] # 3 people the user follows
 
-#calling_first_tweet = api.user_timeline(twitter_id,count=100)[0]
+calling_first_tweet = api.user_timeline(twitter_id,count=5)[0]
 
-#first_cut = str(calling_first_tweet).split("text=u")[1]
+first_cut = str(calling_first_tweet).split("text=u")[1]
 # line above cuts information before tweet
 
-#print str(first_cut).split("in_reply_to_status_id=")[0]
+save += str(first_cut).split("in_reply_to_status_id=")[0]
 # line above cuts after tweet and corresponding link
+
+calling_second_tweet = api.user_timeline(twitter_id,count=5)[1]
+
+first_cut = str(calling_second_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_third_tweet = api.user_timeline(twitter_id,count=5)[2]
+
+first_cut = str(calling_third_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_fourth_tweet = api.user_timeline(twitter_id,count=5)[3]
+
+first_cut = str(calling_fourth_tweet).split("text=u")[1]
+# line above cuts information before tweet
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_fifth_tweet = api.user_timeline(twitter_id,count=5)[4]
+
+first_cut = str(calling_fifth_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_sixth_tweet = api.user_timeline(twitter_id,count=6)[5]
+
+first_cut = str(calling_sixth_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_seventh_tweet = api.user_timeline(twitter_id,count=7)[6]
+
+first_cut = str(calling_seventh_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_eigth_tweet = api.user_timeline(twitter_id,count=8)[7]
+
+first_cut = str(calling_eigth_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_ninth_tweet = api.user_timeline(twitter_id,count=9)[8]
+
+first_cut = str(calling_ninth_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+calling_tenth_tweet = api.user_timeline(twitter_id,count=10)[9]
+
+first_cut = str(calling_tenth_tweet).split("text=u")[1]
+# line above cuts information before tweet
+
+save += str(first_cut).split("in_reply_to_status_id=")[0]
+# line above cuts after tweet and corresponding link
+
+with open('data.txt','w') as writeFile:
+    writeFile.write(str(save))
